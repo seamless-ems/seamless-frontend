ef18f351645002c63e385a1c1aa81e9a5acf4854
itsamejms <james.twose@alliander.com>
2026-01-23 18:31:53 +0000

    adding code to make the app a PWA

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,12 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Register service worker for basic PWA support
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js')
+			.then((reg) => console.log('Service worker registered:', reg))
+			.catch((err) => console.warn('Service worker registration failed:', err));
+	});
+}
